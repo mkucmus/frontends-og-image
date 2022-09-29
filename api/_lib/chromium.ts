0f9975e18d9b09056f93a741b6915cd7ff99e609
mkucmus <maciejkucmus@gmail.com>
2022-09-29 14:21:05 +0200

    chore: check new size

--- api/_lib/chromium.ts
+++ api/_lib/chromium.ts
@@ -20,7 +20,7 @@ export async function getScreenshot(
   isDev: boolean
 ) {
   const page = await getPage(isDev);
-  await page.setViewport({ width: 2400, height: 1254 });
+  await page.setViewport({ width: 2248, height: 1285 });
   await page.setContent(html);
   const file = await page.screenshot({
     type,
