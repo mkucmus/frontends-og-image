afb1239513674da08a6bf3c68980356bce16d455
mkucmus <maciejkucmus@gmail.com>
2022-11-17 09:42:48 +0100

    chore: fix

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -113,7 +113,7 @@ export function getHtml(parsedReq: ParsedRequest) {
   let images = [
     "https://user-images.githubusercontent.com/5596960/192964345-af9ae6ee-88d0-4be7-9edb-e1e787dd2741.png",
   ];
-  if (customImageUrl) {
+  if (typeof customImageUrl === "string") {
     images = [customImageUrl];
   }
 
